f7c29ceae447f16146512d509af5f032feff444d
Jhonaz Rey Baylon <jhonazreybaylon@gmail.com>
2026-09-04 13:16:54 +0800

    - Change color for header and navigation bottom - Create a Projects page section

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 const typed = new Typed('.typed-text', {
     strings: ['UI/UX Design', 'Frontend Developer'],
-    typeSpeed: 100,
-    backSpeed: 100,
-    backDelay: 100,
+    typeSpeed: 50,
+    backSpeed: 50,
+    backDelay: 900,
     loop: true
 });
 
