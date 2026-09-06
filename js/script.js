d444742f539befda24101a076b674a2d85643858
Jhonaz Rey Baylon <jhonazreybaylon@gmail.com>
2026-09-06 18:48:14 +0800

    Make navigation header and button interactive

--- js/script.js
+++ js/script.js
@@ -6,4 +6,33 @@ const typed = new Typed('.typed-text', {
     loop: true
 });
 
+// Navigation buttom active links
+const navLinks = document.querySelectorAll('header nav ul li a, .nav-buttom a');
+const sections = document.querySelectorAll('section[id]');
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+
+            navLinks.forEach(link => {
+                link.classList.remove('active');
+            });
+
+            const activeLinks = document.querySelectorAll(
+                `a[href="#${entry.target.id}"]`
+            );
+
+            activeLinks.forEach(link => {
+                link.classList.add('active');
+            });
+        }
+    });
+}, {
+    threshold: 0.5
+});
+
+sections.forEach(section => {
+    observer.observe(section);
+});
+
 
